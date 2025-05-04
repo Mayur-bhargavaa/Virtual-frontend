98dc121f381ead57d2088ee0c243e6bd0490fb7c
Mayur Bhargava <mayurbhargava026@gmail.com>
2025-05-04 13:33:21 +0530

    update

--- src/pages/Aboutus/Aboutus.jsx
+++ src/pages/Aboutus/Aboutus.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import "./AboutUs.css";
+import "./Aboutus.css";
 import Footer from "../../components/Footer/Footer";
 
 const AboutUs = () => {
